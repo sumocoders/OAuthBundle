24ba08e064bcf63dc4d773bad45782f87cc9e7b9
Abraham <abraham@sumocoders.be>
2026-01-26 18:38:35 +0100

    Symfony route attribute instead of annotation

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,7 @@
 namespace SumoCoders\OAuthBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class SecurityController extends AbstractController
 {
